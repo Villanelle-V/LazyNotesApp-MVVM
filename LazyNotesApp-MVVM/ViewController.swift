d2fcda74dadbb9089212440425d6a6df1fec161e
Polina Reshetilo <polina.reshetilo@gmail.com>
2022-09-29 10:53:14 -0400

    Initial Commit

---
+++ LazyNotesApp-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LazyNotesApp-MVVM
+//
+//  Created by Polina on 2022-09-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
